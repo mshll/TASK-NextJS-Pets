321bf2a2d3076a649041c6a28d5409cb742fb34e
Octowl <alsaff1987@gmail.com>
2024-10-02 12:32:16 +0300

    solve pet button

--- src/app/components/PetItem.js
+++ src/app/components/PetItem.js
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import { useState } from "react";
 
 const btnStyle = "m-4 p-2 bg-palette-primary text-white rounded-sm font-primary font-semibold  hover:bg-palette-dark"
 
 function PetItem({ pet }) {
+  const [showGif, setShowGif] = useState(false);
+
   return (
     <div className="h-120 w-72 rounded shadow-lg mx-auto border border-palette-lighter">
       <div className="h-72 border-b-2 border-palette-lighter relative">
         <Image
           alt={pet.name}
-          src={pet.image}
+          src={showGif ? pet.image2 : pet.image}
           className="transform duration-500 ease-in-out hover:brightness-75"
           fill
           sizes="30vw" />
@@ -21,7 +24,7 @@ function PetItem({ pet }) {
           {pet.description}
         </div>
         <div className="grid grid-cols-2 gap-2">
-          <button type="button" className={btnStyle}>
+          <button type="button" onClick={() => setShowGif(!showGif)} className={btnStyle}>
             Pet
           </button>
           <button type="button" className={btnStyle}>
